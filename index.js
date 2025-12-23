33b40a73fd59980f2f71746959c4a98000bf1699
sonidinesh1648978-dotcom <soni.dinesh1648978@gmail.com>
2025-12-24 03:19:00 +0530

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import express from "express";
-import fetch from "node-fetch";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.json({ status: "backend running" });
 });
 
